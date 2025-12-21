e5c500f21e1e07b1c87d1ee300466c9763f3615e
uhbbang3 <ansdnjswjd33@gmail.com>
2025-12-21 12:36:34 +0900

    [Feat] Player 걷기, 달리기
    Input System 사용
    WASD 걷기
    WASD + Left Shift 달리기

---
+++ Assets/01 Scripts/Player/PlayerMove.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    private InputActions _inputActions;
+    private Rigidbody _rb;
+
+    private Vector2 _moveDirection;
+    private bool _isRun;
+
+    [SerializeField] private float _walkSpeed;
+    [SerializeField] private float _runSpeed;
+
+    private void Awake()
+    {
+        _inputActions = new InputActions();
+        _rb = GetComponent<Rigidbody>();
+
+        _moveDirection = Vector2.zero;
+        _isRun = false;
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Player.Enable();
+
+        _inputActions.Player.Move.performed += OnMovePerformed;
+        _inputActions.Player.Move.canceled += OnMoveCanceled;
+
+        _inputActions.Player.Run.performed += OnRunPerformed;
+        _inputActions.Player.Run.canceled += OnRunCanceled;
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Player.Move.performed -= OnMovePerformed;
+        _inputActions.Player.Move.canceled -= OnMoveCanceled;
+
+        _inputActions.Player.Run.performed -= OnRunPerformed;
+        _inputActions.Player.Run.canceled -= OnRunCanceled;
+
+        _inputActions.Player.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        float speed = _isRun ? _runSpeed : _walkSpeed;
+        _rb.linearVelocity = new Vector3(_moveDirection.x * speed, 0f, _moveDirection.y * speed);
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>().normalized;
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        _moveDirection = Vector2.zero;
+    }
+
+    private void OnRunPerformed(InputAction.CallbackContext context)
+    {
+        _isRun = true;
+    }
+    private void OnRunCanceled(InputAction.CallbackContext context)
+    {
+        _isRun = false;
+    }
+}
