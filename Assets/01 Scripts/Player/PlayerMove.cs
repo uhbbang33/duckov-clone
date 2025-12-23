360d49dc0c70daaad2626364c7ec7f6c88a99c8c
uhbbang3 <ansdnjswjd33@gmail.com>
2025-12-23 16:58:48 +0900

    [Feat] [Fix] 플레이어 구르기 구현 / 잘못된 이동 방향 수정
    플레이어 조작
    - 구르기 조작 및 애니메이션 구현
    - 회전 자연스럽게 수정
    - 달릴 때 바라보는 방향으로 회전하도록 수정
    
    PlayerLook.cs 삭제 - PlayerMove.cs에 병합
    
    SampleScene에 임시 객체 배치

--- Assets/01 Scripts/Player/PlayerMove.cs
+++ Assets/01 Scripts/Player/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,34 +8,163 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody _rb;
     private Animator _anim;
 
-    private Vector2 _moveDirection;
+    private Vector2 _moveInput;
+    private Vector2 _mousePosition;
+    private Vector3 _lookDirection;
+    private Vector3 _rollDirection;
+    private float _rollCoolTime;
     private bool _isRun;
+    private bool _isRoll;
 
     [SerializeField] private float _walkSpeed;
     [SerializeField] private float _runSpeed;
+    [SerializeField] private float _rollSpeed;
+    [SerializeField] private float _rollTime;
+    [SerializeField] private float _rollMaxCoolTime;
+
+    [Tooltip("0.1 - 1.0")]
+    [SerializeField] private float _mouseTurnSpeed;
+    [Tooltip("0.1 - 1.0")]
+    [SerializeField] private float _runTurnSpeed;
+
+    private WaitForSeconds _waitForRoll;
+
+    #region MonoBehaviour
 
     private void Awake()
     {
-        _inputActions = new InputActions();
-        _rb = GetComponent<Rigidbody>();
-        _anim = GetComponent<Animator>();
-
-        _moveDirection = Vector2.zero;
-        _isRun = false;
+        Initialize();
     }
 
     private void OnEnable()
     {
         _inputActions.Player.Enable();
 
+        SubscribeInputActions();
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeInputActions();
+
+        _inputActions.Player.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 dir = new();
+        float speed = 0f;
+
+        if (_isRoll)
+        {
+            dir = _rollDirection;
+            speed = _rollSpeed;
+        }
+        else
+        {
+            dir = SetDirection(_moveInput);
+            speed = _isRun ? _runSpeed : _walkSpeed;
+        }
+
+        if (dir.sqrMagnitude > 0.01f)
+            _rb.linearVelocity = new Vector3(dir.x * speed, _rb.linearVelocity.y, dir.z * speed);
+        else // TODO : Temp code - player slip
+            _rb.linearVelocity = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (_rollCoolTime > 0f)
+            _rollCoolTime -= Time.deltaTime;
+
+        LookAtMouse(_mousePosition);
+    }
+
+    #endregion MonoBehaviour
+
+    private void Initialize()
+    {
+        _inputActions = new InputActions();
+
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+
+        _moveInput = Vector2.zero;
+        _mousePosition = Vector2.zero;
+        _lookDirection = Vector3.zero;
+        _rollDirection = Vector3.zero;
+        _isRun = false;
+        _isRoll = false;
+
+        _waitForRoll = new WaitForSeconds(_rollTime);
+    }
+
+    private void LookAtMouse(Vector2 mousePos)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f))
+        {
+            Vector3 dir = hit.point - transform.position;
+            dir.y = 0f;
+
+            if (dir.sqrMagnitude > 0.01f && !_isRoll)
+            {
+                Vector3 lookDir = dir.normalized;
+                _lookDirection = lookDir;
+
+                if (!_isRoll)
+                {
+                    Quaternion targetRotation;
+                    if (_isRun)
+                    {
+                        Vector3 runDir = SetDirection(_moveInput);
+                        if (runDir.sqrMagnitude > 0.01f)
+                        {
+                            targetRotation = Quaternion.LookRotation(runDir);
+                            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, _runTurnSpeed);
+                        }
+                    }
+                    else
+                    {
+                        targetRotation = Quaternion.LookRotation(lookDir);
+                        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, _mouseTurnSpeed);
+                    }
+                }
+            }
+        }
+    }
+
+    private Vector3 SetDirection(Vector2 input)
+    {
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camRight = Camera.main.transform.right;
+
+        camForward.y = 0f;
+        camRight.y = 0f;
+
+        camForward.Normalize();
+        camRight.Normalize();
+
+        return camForward * input.y + camRight * input.x;
+    }
+
+    #region Input System
+
+    private void SubscribeInputActions()
+    {
         _inputActions.Player.Move.performed += OnMovePerformed;
         _inputActions.Player.Move.canceled += OnMoveCanceled;
 
         _inputActions.Player.Run.performed += OnRunPerformed;
         _inputActions.Player.Run.canceled += OnRunCanceled;
+
+        _inputActions.Player.Roll.performed += OnRollPerformed;
+
+        _inputActions.Player.Look.performed += OnLook;
     }
 
-    private void OnDisable()
+    private void UnsubscribeInputActions()
     {
         _inputActions.Player.Move.performed -= OnMovePerformed;
         _inputActions.Player.Move.canceled -= OnMoveCanceled;
@@ -42,24 +172,20 @@ public class PlayerMove : MonoBehaviour
         _inputActions.Player.Run.performed -= OnRunPerformed;
         _inputActions.Player.Run.canceled -= OnRunCanceled;
 
-        _inputActions.Player.Disable();
-    }
+        _inputActions.Player.Roll.performed -= OnRollPerformed;
 
-    private void FixedUpdate()
-    {
-        float speed = _isRun ? _runSpeed : _walkSpeed;
-        _rb.linearVelocity = new Vector3(_moveDirection.x * speed, 0f, _moveDirection.y * speed);
+        _inputActions.Player.Look.performed -= OnLook;
     }
 
     private void OnMovePerformed(InputAction.CallbackContext context)
     {
-        _moveDirection = context.ReadValue<Vector2>().normalized;
+        _moveInput = context.ReadValue<Vector2>().normalized;
         _anim.SetBool("IsWalk", true);
     }
 
     private void OnMoveCanceled(InputAction.CallbackContext context)
     {
-        _moveDirection = Vector2.zero;
+        _moveInput = Vector2.zero;
         _anim.SetBool("IsWalk", false);
     }
 
@@ -73,5 +199,49 @@ public class PlayerMove : MonoBehaviour
     {
         _isRun = false;
         _anim.SetBool("IsRun", false);
+    }
+
+    private void OnRollPerformed(InputAction.CallbackContext context)
+    {
+        if (_isRoll || _rollCoolTime > 0f)
+            return;
+
+        _isRoll = true;
+
+        // dir - move
+        if (_moveInput != Vector2.zero)
+        {
+            Vector3 dir = SetDirection(_moveInput);
+            if (dir.sqrMagnitude > 0.01f)
+            {
+                transform.rotation = Quaternion.LookRotation(dir);
+            }
+            _rollDirection = dir;
+        }
+        else // dir - mouse
+        {
+            _rollDirection = _lookDirection;
+        }
+
+        StartCoroutine(RollRoutine());
+
+        _anim.SetTrigger("Roll");
+    }
+
+
+    private void OnLook(InputAction.CallbackContext context)
+    {
+        _mousePosition = context.ReadValue<Vector2>();
+    }
+
+    #endregion Input System
+
+    private IEnumerator RollRoutine()
+    {
+        yield return _waitForRoll;
+
+        _rollCoolTime = _rollMaxCoolTime;
+
+        _isRoll = false;
     }
 }
