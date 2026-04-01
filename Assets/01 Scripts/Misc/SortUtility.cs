8db03e216e8f548ebb9b2a516115f7fb538990df
uhbbang3 <ansdnjswjd33@gmail.com>
2026-04-01 14:36:16 +0900

    [Feat] 아이템 정렬 시 같은 ID 합치기 기능 구현

--- Assets/01 Scripts/Misc/SortUtility.cs
+++ Assets/01 Scripts/Misc/SortUtility.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,28 +17,64 @@ public static class SortUtility
     {
         List<ItemSlot> slots = target.GetSortableSlots();
 
-        var items = slots
+        var originItems = slots
             .Where(slot => slot.CurrentItem != null) // 빈 슬롯 제외
-            .OrderBy(slot => _itemTypePriority.GetValueOrDefault(slot.CurrentItem.Type, int.MaxValue)) // Item Type 오름차순으로
-            .ThenByDescending(slot => slot.CurrentItem.Weight) // 무게 내림차순
             .Select(slot => (slot.CurrentItem, slot.Quantity, slot.UI.LinkedQuickSlot))
             .ToList();
 
+        var mergedItems = MergeItem(originItems);
+
+        var sortedItems = mergedItems
+            .OrderBy(slot => _itemTypePriority.GetValueOrDefault(slot.item.Type, int.MaxValue)) // Item Type 오름차순으로
+            .ThenByDescending(slot => slot.item.Weight) // 무게 내림차순
+            .ToList();
+
+        // 초기화
         foreach (ItemSlot slot in slots)
         {
             slot.UI.LinkedQuickSlot = null;
             slot.SubtractItem(slot.Quantity);
         }
 
-        for (int i = 0; i < items.Count; ++i)
+        for (int i = 0; i < sortedItems.Count && i <slots.Count; ++i)
         {
-            int quantity = items[i].Quantity;
+            int quantity = sortedItems[i].qauntity;
 
-            slots[i].AddItem(items[i].CurrentItem, ref quantity);
-
-            slots[i].UI.LinkedQuickSlot = items[i].LinkedQuickSlot;
+            slots[i].AddItem(sortedItems[i].item, ref quantity);
+            slots[i].UI.LinkedQuickSlot = sortedItems[i].likedQuickSlot;
         }
 
         target.OnSortCompleted();
+    }
+
+    private static List<(Item item, int qauntity, QuickSlot likedQuickSlot)> MergeItem
+        (IEnumerable<(Item currentItem, int quantity, QuickSlot linkedQuickSlot)> originItems)
+    {
+        var groupedById = originItems
+            .GroupBy(i => i.currentItem.ID)
+            .Select(s => (
+            s.First().currentItem,
+            s.Sum(i => i.quantity),
+            s.First().linkedQuickSlot
+            ));
+
+        var result = new List<(Item, int, QuickSlot)>();
+
+        foreach (var (item, totalQuantity, linkedQuickSlot) in groupedById)
+        {
+            int maxStackSize = (int)item.MaxStackSize;
+            int remainQuantity = totalQuantity;
+            bool isFirst = true;
+
+            while(remainQuantity > 0)
+            {
+                int stackSize = Math.Min(remainQuantity, maxStackSize);
+                result.Add((item, stackSize, isFirst ? linkedQuickSlot : null));
+                remainQuantity -= stackSize;
+                isFirst = false;
+            }
+        }
+
+        return result;
     }
 }
