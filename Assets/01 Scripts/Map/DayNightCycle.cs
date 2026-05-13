04edd63ee90a81246a991e2b32efba0a039105ff
uhbbang3 <ansdnjswjd33@gmail.com>
2026-05-13 15:22:23 +0900

    [Feat] Field Scene 낮/밤 구현

---
+++ Assets/01 Scripts/Map/DayNightCycle.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [Range(0f, 24f)]
+    [SerializeField] private float _currentTime;
+    [SerializeField] private float _dayDurationSeconds; // 실제 초 기준 하루 길이
+    [SerializeField] private Light _sunLight;
+    [SerializeField] private Gradient _sunColor; // 시간대별 색상
+    [SerializeField] private float _lightYRotate = -30f;
+
+    private const float _hoursPerDay = 24f;
+
+    private void Update()
+    {
+        _currentTime += (_hoursPerDay / _dayDurationSeconds) * Time.deltaTime;
+        if (_currentTime >= _hoursPerDay)
+            _currentTime = 0f;
+
+        float t = _currentTime / _hoursPerDay;
+
+        // 빛(태양) 회전
+        float sunAngle = t * 360f - 90f;
+        _sunLight.transform.rotation = Quaternion.Euler(sunAngle, _lightYRotate, 0f);
+
+        _sunLight.color = _sunColor.Evaluate(t);
+    }
+}
