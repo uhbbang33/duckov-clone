282116f2ec54cbeb217af6ecfa343dcaea107dcf
uhbbang3 <ansdnjswjd33@gmail.com>
2026-05-23 23:49:03 +0900

    [Fix] Camera Shake 방향 수정

--- Assets/01 Scripts/Misc/CameraShakeController.cs
+++ Assets/01 Scripts/Misc/CameraShakeController.cs
@@ -7,12 +7,17 @@ public class CameraShakeController : MonoBehaviour
     [SerializeField] private float _speedMultiplier = 0.001f;
     [SerializeField] private float _impulseDuration = 0.1f;
 
-    public void ShakeOnFire(Vector3 shakeDirection, float speed)
+    public void ShakeOnFire(Vector3 fireDir, float speed)
     {
         float force = speed * _speedMultiplier;
-        
-        _impulseSource.ImpulseDefinition.ImpulseDuration = _impulseDuration;
 
-        _impulseSource.GenerateImpulseWithVelocity(shakeDirection * force);
+        Transform camTransform = Camera.main.transform;
+        float rightComponent = Vector3.Dot(fireDir, camTransform.right);
+        float upComponent = Vector3.Dot(fireDir, camTransform.up);
+
+        Vector3 shakeDir = (camTransform.right * rightComponent + camTransform.up * upComponent).normalized;
+
+        _impulseSource.ImpulseDefinition.ImpulseDuration = _impulseDuration;
+        _impulseSource.GenerateImpulseWithVelocity(shakeDir * force);
     }
 }
