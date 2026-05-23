803d965feca18107b7118db0d1d927fb36aa9f99
uhbbang3 <ansdnjswjd33@gmail.com>
2026-05-23 23:06:30 +0900

    [Feat] 카메라 흔들림 속도 조절 구현

--- Assets/01 Scripts/Misc/CameraShakeController.cs
+++ Assets/01 Scripts/Misc/CameraShakeController.cs
@@ -5,10 +5,14 @@ public class CameraShakeController : MonoBehaviour
 {
     [SerializeField] private CinemachineImpulseSource _impulseSource;
     [SerializeField] private float _speedMultiplier = 0.001f;
+    [SerializeField] private float _impulseDuration = 0.1f;
 
     public void ShakeOnFire(Vector3 shakeDirection, float speed)
     {
         float force = speed * _speedMultiplier;
+        
+        _impulseSource.ImpulseDefinition.ImpulseDuration = _impulseDuration;
+
         _impulseSource.GenerateImpulseWithVelocity(shakeDirection * force);
     }
 }
